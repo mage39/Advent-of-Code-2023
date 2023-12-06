6357440913e7ce2e668b5ef56c908a37c094d310
mage39 <106197576+mage39@users.noreply.github.com>
2023-12-06 14:24:50 -0600

    segfault. damn.

--- aoc3.c
+++ aoc3.c
@@ -3,14 +3,14 @@
 #include <string.h>
 
 int main() {
-	char buff[3][160] = {0}
+	char buff[3][160] = {0};
 	char where[8][2] = {
 		{-1, -1}, {-1, 0}, {-1, 1},
-		{0, -1}, {0, 1},
-		{1, -1}, {1, 0}, {1, 1}
+		{0,  -1}, 		   {0,  1},
+		{1,  -1}, {1,  0}, {1,  1}
 	};
-	FILE *input = fopen("aoc3input.txt". "r");
-	int sum = 0, first = 0;
+	FILE *input = fopen("aoc3input.txt", "r");
+	int sum = 0, row = 0, lines = 2;
 
 	if (!input) {
 		printf("WTF");
@@ -21,9 +21,48 @@ int main() {
 	fgets(buff[1], 160, input);
 
 	while(fgets(buff[2], 160, input)) {
-		for (int i = 0; i < 160 && buff[first][i]; i++) {
-			switch (buff[first][i]) {
-				case '1'...'9': case '.':
-					break;
-				default:
+		lines++;
+		start:
+		for (int i = 0; i < 160 && buff[row][i]; i++) {
+			switch (buff[row][i]) {
+			case '1'...'9': case '.':
+				break;
+			default:
+				for (int j = 0; j < 8; j++) {
+					char num[4] = {0};
+					if (row < 1 && i < 1 && (j < 4 || j == 5)) break;
+					if (row > 1 && i < 1 && (j == 0 || j == 3 || j > 4)) break;
+					if (row < 1 && i > 138 && (j < 4 || j == 5)) break;
+					if (row > 1 && i < 138 && (j == 0 || j == 3 || j > 4)) break;
 
+					switch (buff[row + where[j][0]][i + where[j][1]]) {
+					case '1'...'9':
+						for (int k = 0; k < 3; k++) {
+							switch (buff[row + where[j][0]][i + where[j][1] * k]) {
+							case '1'...'9':
+								break;
+							default:
+								for (int l = 0; l >= k; k++) {
+									num[l] = buff[row + where[j][0]][i + where[j][1] * k + l];
+								}
+							}
+						}
+					}
+					sum += atoi(num);
+				}
+			}
+		}
+		
+		if (!row || lines > 139) {
+			row++;
+			goto start;
+		} else {
+			memcpy(buff[0], buff[1], 160);
+			memcpy(buff[1], buff[0], 160);
+		}
+	}
+
+	printf("%d", sum);
+	fclose(input);
+	return 0;
+}
